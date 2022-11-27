d4cc25b3e0e8f6f29ec8bee3b5aafc5b803ad98d
Aadithya Seshadri <aadithyaseshadri@Aadithyas-MacBook-Air.local>
2022-11-27 20:01:36 +0530

    Aligned Add Contact widgets

--- lib/addcontacts.dart
+++ lib/addcontacts.dart
@@ -22,38 +22,45 @@ class _AddContactsState extends State<AddContacts> {
         backgroundColor: Colors.cyan[700],
       ),
       body: SingleChildScrollView(
-        child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(20),
-                  child: Text(
-                    "Add Emergency Contacts",
-                    style: GoogleFonts.urbanist(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30.0,
-                        color: Colors.black
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                "Add Emergency Contacts",
+                style: GoogleFonts.urbanist(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30.0,
+                    color: Colors.black
                 ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(40.0),
+              color: Colors.teal,
+              child: Text('Display contacts & dustbin icon here'),
+            ),
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
                 SizedBox(
-                  width: 300,
-                  child: TextField(
-                    decoration: InputDecoration(
-                        hintText: 'Enter 10-digit mobile number',
-                        contentPadding: const EdgeInsets.all(15),
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(30)
-                        )),
-                    keyboardType: TextInputType.number,
-                    inputFormatters: [
-                      FilteringTextInputFormatter.digitsOnly
-                    ],
-                    //onChanged: (val) => print(val),
-                  )
+                    width: 300,
+                    child: TextField(
+                      decoration: InputDecoration(
+                          hintText: 'Enter 10-digit mobile number',
+                          contentPadding: const EdgeInsets.all(15),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(30)
+                          )),
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly
+                      ],
+                      //onChanged: (val) => print(val),
+                    )
                 ),
                 ElevatedButton(
                   onPressed: () {},
@@ -63,34 +70,36 @@ class _AddContactsState extends State<AddContacts> {
                     padding: EdgeInsets.all(10),
                     backgroundColor: Colors.white, // <-- Button color
                     foregroundColor: Colors.teal, // <-- Splash color
+                    side: BorderSide(width: 3.0, color: Colors.teal)
                   ),
                 ),
-                SizedBox(height: 10),
-                Center(
-                  child: ElevatedButton.icon(
-                    onPressed: () {
-                      Navigator.pushNamed(context, '/setreachtime');
-                    },
-                    icon: Icon(Icons.navigate_next),
-                    label: Text(
-                      "Proceed",
-                      style: GoogleFonts.urbanist(
-                        fontSize: 19.0,
-                        color: Colors.black,
-                        fontWeight: FontWeight.w500,
-                      ),
-                    ),
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        foregroundColor: Colors.black,
-                        shape: StadiumBorder(),
-                        minimumSize: const Size(250, 40),
-                        maximumSize: const Size(250, 40),
-                        side: BorderSide(width: 2.0, color: Colors.black54)
-                    ),),
-                )
               ],
+            ),
+            SizedBox(height: 10),
+            Center(
+              child: ElevatedButton.icon(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/setreachtime');
+                },
+                icon: Icon(Icons.navigate_next),
+                label: Text(
+                  "Proceed",
+                  style: GoogleFonts.urbanist(
+                    fontSize: 19.0,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    foregroundColor: Colors.black,
+                    shape: StadiumBorder(),
+                    minimumSize: const Size(250, 40),
+                    maximumSize: const Size(250, 40),
+                    side: BorderSide(width: 2.0, color: Colors.black54)
+                ),),
             )
+          ],
         ),
       ),
     );
